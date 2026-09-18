f3eef5f26ed478d62afc7cb87ff52bc7a0295224
TomDeresh <tom.deresh@gmail.com>
2026-09-18 10:53:03 +0300

    Implement game rating calculator with Streamlit
    Add Streamlit app for game rating calculation

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
+import streamlit as st
 
+st.title("tomProject")
+
+user_prompt = st.number_input("Enter a game rating")
+w = 110212.26226094973
+b = -45899.861928343424
+if user_prompt:
+    response = w * user_prompt + b
+
+    st.subheader("Response:")
+    st.write(response)
